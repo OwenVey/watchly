2cd5bb2c0759fee15529ca7015a029d88fa478ad
Owen Vey <owen.vey@gmail.com>
2024-10-25 16:51:14 -0500

    load lower quality backdrop

--- src/routes/(movies)/movies_.$movieId.tsx
+++ src/routes/(movies)/movies_.$movieId.tsx
@@ -99,7 +99,7 @@ function Movie() {
         <div className="absolute top-0 right-0 left-0 -z-10">
           <img
             className="h-[45rem] w-full object-cover opacity-10 blur-sm"
-            src={getTmdbImage('backdrop', movie.backdrop_path, 'w1280')}
+            src={getTmdbImage('backdrop', movie.backdrop_path, 'w300')}
             alt={`backdrop image for ${movie.title}`}
           />
           <div className="absolute right-0 -bottom-4 left-0 h-1/2 bg-gradient-to-t from-gray-1" />
