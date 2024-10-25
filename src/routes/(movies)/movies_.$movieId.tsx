6825c375c7c179e3d8481bdebcef9df381d505da
Owen Vey <owen.vey@gmail.com>
2024-10-25 16:32:48 -0500

    improve movie backdrop gradient and sizing

--- src/routes/(movies)/movies_.$movieId.tsx
+++ src/routes/(movies)/movies_.$movieId.tsx
@@ -99,11 +99,11 @@ function Movie() {
       {movie.backdrop_path && (
         <div className="absolute top-0 right-0 left-0 -z-10">
           <Image
-            className="min-h-[40rem] w-full object-cover opacity-10 blur-xs"
+            className="h-[45rem] w-full object-cover opacity-10 blur-sm"
             src={getTmdbImage('backdrop', movie.backdrop_path, 'w1280')}
             alt={`backdrop image for ${movie.title}`}
           />
-          <div className="absolute right-0 bottom-0 left-0 h-1/2 bg-gradient-to-t from-gray-1" />
+          <div className="absolute right-0 -bottom-4 left-0 h-1/2 bg-gradient-to-t from-gray-1" />
         </div>
       )}
 
