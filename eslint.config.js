27d25006004487c4f33273c855299143b64ef724
Owen Vey <owen.vey@gmail.com>
2025-09-02 19:55:33 -0500

    cleanup eslint setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,51 +1,49 @@
-import js from '@eslint/js';
+import eslint from '@eslint/js';
 import pluginQuery from '@tanstack/eslint-plugin-query';
 import pluginRouter from '@tanstack/eslint-plugin-router';
 import eslintConfigPrettier from 'eslint-config-prettier';
-import react from 'eslint-plugin-react';
+import reactPlugin from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig } from 'eslint/config';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default defineConfig(
+  eslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  pluginRouter.configs['flat/recommended'],
+  pluginQuery.configs['flat/recommended'],
+  reactHooks.configs['recommended-latest'],
+  reactRefresh.configs.recommended,
+  reactPlugin.configs.flat.recommended,
+  reactPlugin.configs.flat['jsx-runtime'], // Add this if you are using React 17+
+  eslintConfigPrettier,
   {
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.recommendedTypeChecked,
-      ...pluginRouter.configs['flat/recommended'],
-      ...pluginQuery.configs['flat/recommended'],
-      eslintConfigPrettier,
-    ],
-    files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
+        projectService: true,
         ecmaFeatures: {
           jsx: true,
         },
-        project: ['./tsconfig.json'],
         tsconfigRootDir: import.meta.dirname,
       },
     },
-    settings: { react: { version: '18.3' } },
-    plugins: {
-      react,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
+  },
+  { ignores: ['dist', 'eslint.config.js'] },
+  {
+    settings: { react: { version: 'detect' } },
     rules: {
-      ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
-      ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': 'off',
       'react/react-in-jsx-scope': 'off',
       '@typescript-eslint/no-misused-promises': 'off',
       'react/prop-types': 'off',
       '@typescript-eslint/no-empty-object-type': 'off',
       '@typescript-eslint/no-unused-vars': 'warn',
+      'no-throw-literal': 'off',
+      '@typescript-eslint/only-throw-error': ['error', { allow: ['Redirect'] }],
     },
   },
 );
