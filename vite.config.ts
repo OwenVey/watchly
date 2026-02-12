84f7729c9dbdcd0c3966f60b8af0b9d46848b1dd
Owen Vey <owen.vey@gmail.com>
2026-02-11 19:16:29 -0600

    add vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,13 @@ import tailwindcss from '@tailwindcss/vite';
 import { devtools } from '@tanstack/devtools-vite';
 import { tanstackRouter } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react';
-import path from 'node:path';
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    tsconfigPaths(),
     devtools({
       consolePiping: {
         enabled: false,
@@ -23,9 +24,4 @@ export default defineConfig({
     }),
     tailwindcss(),
   ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
 });
